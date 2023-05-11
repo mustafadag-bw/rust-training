65785f65a8c3dd991510f66d6ba4f4850ffa7ccd
mustafa dag <mustafa.dag@beakwise.com>
2023-05-11 13:51:36 +0300

    chapter 12: CL Program is done and .env added to .gitignore

---
+++ 12-commandline-program/minigrep/src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, process};
+
+use minigrep::Config;
+
+// cargo run -- search_param
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+    // args = ["target/debug/minigrep", "needle", "haystack"]
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
